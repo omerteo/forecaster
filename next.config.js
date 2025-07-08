320ac493b174cca1056bca651113c52ab769435e
Omar Saad <Omer.saad@teo-intl.com>
2025-07-08 14:56:29 +0500

    Prisma agaiun

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-// next.config.js
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
   },
 };
